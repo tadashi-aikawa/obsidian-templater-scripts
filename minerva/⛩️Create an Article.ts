0bdae8192a567fa22d11d9b5fee5babcf2f1b3c6
tadashi-aikawa <syou.maman@gmail.com>
2023-11-15 20:38:30 +0900

    Article作成スクリプトを追加

---
+++ minerva/⛩️Create an Article.ts
@@ -0,0 +1,30 @@
+///<%*
+const T = tp.user.fryTempura();
+const title = await T.showInputDialog("Articleのタイトルを入力してください");
+if (!title) {
+  throw new Error("タイトルは必須です");
+}
+
+const fp = `📘Articles/📘${title}.md`;
+if (await T.fileExists(fp)) {
+  throw new Error(`⚠️Error: ${fp} is already existed.`);
+}
+
+const today = T.now("YYYY-MM-DD");
+
+const f = await T.createFile(
+  fp,
+  `[[📒Articles]] > [[📒2023 Articles]]
+
+![[${today}.jpg|cover-picture]]
+`
+);
+
+await T.openFile(f.path);
+T.addProperties({
+  created: today,
+  updated: today,
+  description: "TODO",
+  cover: `📘Articles/attachments/${today}.jpg`,
+});
+///%>
